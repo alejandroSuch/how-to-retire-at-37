4cee52599728abb5a0436dbbadadf51c2fb9e0bd
Alejandro Such <alejandro.such@gitkraken.com>
2022-11-24 10:01:56 +0100

    hello, world

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("TO-DO LIST")
+	fmt.Println("==========")
+	fmt.Println("")
+	fmt.Println("✗ Do the laundry")
+	fmt.Println("✓ Learn golang")
+	fmt.Println("✗ Have a snack")
+}
